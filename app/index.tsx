5b1c8e2c2b2ae5a1de40f8ac3ddca57418185f99
João Paulo Abdala Bohaczk <okiabdala@gmail.com>
2024-10-23 20:58:36 -0300

    ajustes

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Href, Link, router,  } from 'expo-router'
 
 
-const generateRandomId = Math.floor(Math.random() * 1000);
+const generateRandomId = () => Math.floor(Math.random() * 1000);
 
 
 export default function index() {
@@ -14,7 +14,7 @@ export default function index() {
       onPress={
         ()=> router.push({
            pathname:"/random", 
-           params: {n : generateRandomId}         
+           params: {n : generateRandomId()}         
           })}
       ></Button>
      <Link href={"/dontKnow"}>
